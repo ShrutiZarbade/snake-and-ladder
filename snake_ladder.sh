aa4ef8cdea17b17652273497c6d69fca4ea35ff8
ShrutiZarbade <shrutizarbade22@gmail.com>
2020-02-21 12:34:35 +0530

    added:the winning condition of a player

--- snake_ladder.sh
+++ snake_ladder.sh
@@ -13,24 +13,32 @@ function Diceroll(){
 		on_dice=$(( RANDOM % 6 + 1 ))
 		echo $on_dice
 		}
-Diceroll
 # function to choice the option between no play,ladder and snake.
 function Tocheck_option(){
-			option=$(( RANDOM % 3 ))
-			echo $option
-			case $option in
-						0)
-							current_pos=$current_pos ;;  # 0 is for no play
+			while [[ $current_pos -le  $winning_pos ]]
+			do
+					option=$(( RANDOM % 3 ))
+					case $option in
+								0)
+									current_pos=$current_pos ;;  # 0 is for no play
 
-						1)
-							current_pos=$(( $current_pos + $on_dice ));;
-							# player position will move ahead by no comes in Diceroll
+								1)
+									current_pos=$(( $current_pos + $on_dice ))
+									# player position will move ahead by no comes in Diceroll
+								;;
 
-						2)
-							current_pos=$(( $current_pos - $on_dice ));;
-							# Player pos will move behind by the num comes on diceroll
-			esac
+								2)
+									current_pos=$(( $current_pos - $on_dice ))
+									# Player pos will move behind by the num comes on diceroll
+								;;
+					esac
+
+					if [ $current_pos -lt 0 ]
+					then
+							current_pos=0
+					fi
+			done
 			echo $current_pos
 			}
-Tocheck_option
+
 
