311f0e6dda68cc7c1712ea15372b86032a280aa5
ShrutiZarbade <shrutizarbade22@gmail.com>
2020-02-21 16:42:50 +0530

    added: the number of die rolls to win

--- snake_ladder.sh
+++ snake_ladder.sh
@@ -12,6 +12,7 @@ current_pos=0
 function Diceroll(){
 		on_dice=$(( RANDOM % 6 + 1 ))
 		echo $on_dice
+		(( dierollcount++ ))
 		}
 
 # function to choice the option between no play,ladder and snake.
@@ -23,7 +24,7 @@ function Tocheck_option(){
 				0)
 					current_pos=$current_pos ;;  # 0 is for no play
 
-				1)
+				1) Diceroll
 					current_pos=$(( $current_pos + $on_dice ))
 					if [[ $current_pos -gt $winning_pos ]]
 					then
@@ -31,19 +32,18 @@ function Tocheck_option(){
 					fi
 					;;
 
-				2)
+				2)Diceroll
 					current_pos=$(( $current_pos - $on_dice ))
+					if [[ $current_pos -lt 0 ]]
+					then
+						current_pos=0
+					fi
 					# Player pos will move behind by the num comes on diceroll
 					;;
 			esac
-
-			if [ $current_pos -lt 0 ]
-			then
-				current_pos=0
-			fi
 		done
 		echo $current_pos
 	}
-
+echo $dierollcount
 
 
