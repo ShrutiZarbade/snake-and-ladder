82ece73400cb63587f32a8f14c1fcd372e3c9d47
Shruti Zarbade <shrutizarbade22@gmail.com>
2020-02-22 11:59:11 +0530

    added:the two player

--- snake_ladder.sh
+++ snake_ladder.sh
@@ -5,8 +5,13 @@ echo "Hello!! $username"
 
 # constants
 winning_pos=100
-#starting_pos=0
-current_pos=0
+starting_pos=0
+#current_pos=0
+
+#variables
+player1_pos=$starting_pos
+player2_pos=$starting_pos
+checkplayer=1
 
 # function to roll the dice randomly
 function Diceroll(){
@@ -16,34 +21,96 @@ function Diceroll(){
 		}
 
 # function to choice the option between no play,ladder and snake.
-function Tocheck_option(){
-		while [[ $current_pos -ne  $winning_pos ]]
-		do
-			option=$(( RANDOM % 3 ))
-			case $option in
+function Tocheck_option()
+	{
+		option=$(( RANDOM % 3 ))
+		echo $option
+		case $option in
 				0)
-					current_pos=$current_pos ;;  # 0 is for no play
+					if [[ $checkplayer -eq 1 ]]
+					then
+				  		player1_pos=$player1_pos
+						echo player1_position is $player1_pos
+					else
+				      player2_pos=$player2_pos
+						echo player2_position is $player2_pos
+					fi
+					;;
 
 				1) Diceroll
-					current_pos=$(( $current_pos + $on_dice ))
-					if [[ $current_pos -gt $winning_pos ]]
+					if [[ $checkplayer -eq 1 ]]
 					then
-						current_pos=$(( $current_pos - $on_dice ))
-					fi
+						player1_pos=$(( $player1_pos + $on_dice ))
+						if [[ $player1_pos -gt $winning_pos ]]
+						then
+							player1_pos=$(( $player1_pos - $on_dice ))
+						fi
+						echo player1_position is $player1_pos
+					else
+						player2_pos=$(( $player2_pos + $on_dice ))
+						if [[ $player2_pos -gt $winning_pos ]]
+                  then
+                     player2_pos=$(( $player2_pos - $on_dice ))
+                  fi
+                  echo player2_position is $player2_pos
+            	fi
 					;;
 
 				2)Diceroll
-					current_pos=$(( $current_pos - $on_dice ))
-					if [[ $current_pos -lt 0 ]]
+					if [[ $checkplayer -eq 1 ]]
 					then
-						current_pos=0
+						player1_pos=$(( $player1_pos - $on_dice ))
+						if [[ $player1_pos -lt $starting_pos ]]
+						then
+							player1_pos=$starting_pos
+						fi
+						echo player1_position is $player1_pos
+					else
+						 player2_pos=$(( $player2_pos - $on_dice ))
+                  if [[ $player2_pos -lt $starting_pos ]]
+                  then
+                     player2_pos=$starting_pos
+                  fi
+						echo player2_position is $player2_pos
 					fi
-					# Player pos will move behind by the num comes on diceroll
 					;;
 			esac
-		done
-		echo $current_pos
 	}
-echo $dierollcount
 
+function player1()
+{
+	echo "player1 turn"
+	Diceroll
+	Tocheck_option
+}
 
+function player2()
+{
+	echo "player2 turn"
+	Diceroll
+	Tocheck_option
+}
+
+function game(){
+	while [[ $player1_pos -ne $winning_pos && $player2_pos -ne $winning_pos ]]
+	do
+		if [[ $checkplayer -eq 1 ]]
+		then
+			player1
+			checkplayer=2
+		elif [[ $checkplayer -eq 2 ]]
+		then
+			player2
+			checkplayer=1
+		fi
+	done
+}
+game
+
+echo $dierollcount 
+if [[ $checkplayer -eq 1 ]]
+then
+	echo Player 2 won
+else
+	echo Player 1 won
+fi
